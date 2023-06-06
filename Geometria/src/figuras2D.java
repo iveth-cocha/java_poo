cf9f4669f13deec3e6a70c4a5c114e08066115e8
iveth <maria.iveth16outlook.es>
2023-06-06 07:31:55 -0500

    inicialización

--- Geometria/src/figuras2D.java
+++ Geometria/src/figuras2D.java
@@ -1,2 +1,20 @@
 public class figuras2D {
+    int nlados;
+    //cosntructor
+    public figuras2D(int nlados) {
+        this.nlados = nlados;
+    }
+    //creacion de setter and getter
+    public int getNlados() {
+        return nlados;
+    }
+
+    public void setNlados(int nlados) {
+        this.nlados = nlados;
+    }
+
+    areaCuadrado(){
+
+    }
+
 }
